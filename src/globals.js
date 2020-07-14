65233adbf069abdc373ac6265f3816b5ac09c2d2
Axel Rodriguez <alejandrom9712@gmail.com>
2020-07-14 15:50:58 -0600

    Prueba de peticion para registrar token de spotify

--- src/globals.js
+++ src/globals.js
@@ -1,4 +1,8 @@
-const spotify_client = "f940d61988a6484fbdcc98a596d479ec"
-const spotify_redirect = "http://localhost:3000/linked-account"
-const api_gateway_url = "https://klsm6mi2h1.execute-api.us-west-2.amazonaws.com/Prod"
-const load_balancer_url = "http://shiftemotionspotifyblancer-378386110.us-west-2.elb.amazonaws.com/"
+const variables = {
+    spotify_client: "f940d61988a6484fbdcc98a596d479ec",
+    spotify_redirect: "http://localhost:3000/linked-account",
+    api_gateway_url: "https://i1jv10s1vb.execute-api.us-east-1.amazonaws.com/Prod",
+    load_balancer_url: "http://shiftemotionspotifyblancer-378386110.us-west-2.elb.amazonaws.com/"
+}
+
+export default variables;
