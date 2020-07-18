3ff1b3a9323028ebff1e96231c67460f14eec28d
Axel Rodriguez <alejandrom9712@gmail.com>
2020-07-18 12:57:59 -0600

    Despliege de historial y recomendaciones anteriores

--- src/Historial/Historial.js
+++ src/Historial/Historial.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Header from './../Layouts/Headers'
-import { Container, Row, Col, Card, Image, ListGroup } from 'react-bootstrap'
+import { Container, Row, Col, Card, Image, ListGroup, Table } from 'react-bootstrap'
 import Axios from 'axios'
 import variables from '../globals'
 
@@ -8,14 +8,15 @@ class Historial extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            isLoading: true
+            isLoading: true,
+            selectedItem: -1,
+            selectedEmotion: ""
         }
         Axios.get(variables.api_gateway_url + "/recomendaciones/historial?email=" + localStorage.getItem("user-email"))
         .then(response => {
             this.setState({
-                 recomendaciones: response.data
+                 recomendaciones: response.data.data
             })
-            console.log(JSON.parse(response.data))
         }).catch(error => {
             console.log(error)
         }).finally(() => {
@@ -40,15 +41,30 @@ class Historial extends Component {
                                             (
                                                 this.state.recomendaciones.map(item => {
                                                     return (
-                                                        <ListGroup.Item>
+                                                        <ListGroup.Item key={item.id} onClick={
+                                                            () => {
+                                                                Axios.get(variables.api_gateway_url + "/recomendaciones/historial/recomendacion?foto=" + item.foto)
+                                                                .then(response => {
+                                                                    this.setState({
+                                                                        selectedItem: response.data,
+                                                                        selectedEmotion: item.emocion
+                                                                    })
+                                                                }).catch(error => {
+                                                                    console.log(error)
+                                                                    this.setState({
+                                                                        selectedItem: -1
+                                                                    })
+                                                                })
+                                                            }
+                                                        }>
                                                             <Row>
-                                                                <Col>
-                                                                <Image 
-                                                                    src={variables.s3_files_url + item.foto}
-                                                                    rounded
-                                                                    height="150" />
+                                                                <Col md={3}>
+                                                                    <Image 
+                                                                        src={variables.s3_files_url + item.foto}
+                                                                        rounded
+                                                                        height="100" />
                                                                 </Col>
-                                                                <Col>
+                                                                <Col md={9}>
                                                                     <Row>
                                                                         <Col>{item.emocion}</Col>
                                                                     </Row>
@@ -71,11 +87,51 @@ class Historial extends Component {
                             </Card>
                         </Col>
                         <Col md="9">
-                            <Card>
-                                <Card.Body>
-                                    Canciones
-                                </Card.Body>
-                            </Card>
+                            {
+                                this.state.selectedItem !== -1 ? (
+                                    <Card>
+                                        <Card.Body>
+                                            <Row>
+                                                <Col md={4}>
+                                                    <Image 
+                                                        src={variables.s3_files_url + this.state.selectedItem.foto}
+                                                        rounded
+                                                        height="100" />
+                                                </Col>
+                                                <Col md={8}>
+                                                    <h2>En ese momento estabas: {this.state.selectedEmotion}</h2>
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Table responsive striped bordered hover>
+                                                    <thead>
+                                                        <tr>
+                                                            <th>Nombre</th>
+                                                            <th>Artista</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        {
+                                                            Array.isArray(this.state.selectedItem.canciones) ? this.state.selectedItem.canciones.map((track) => {
+                                                                return (<tr key={track.id} >
+                                                                    <td>{track.nombre}</td>
+                                                                    <td>{track.artista}</td>
+                                                                </tr>)
+                                                                
+                                                        }) : (<tr>
+                                                            <td>
+
+                                                            </td>
+                                                        </tr>)
+                                                        }
+                                                    </tbody>
+                                                </Table>
+                                            </Row>
+                                        </Card.Body>
+                                    </Card>
+                                ) : null
+                            }
+                            
                         </Col>
                     </Row>
                 </Container>
