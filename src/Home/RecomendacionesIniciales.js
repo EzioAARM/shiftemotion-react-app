6410b0a56810d8373514fcd6f69877c78b06661b
Axel Rodriguez <alejandrom9712@gmail.com>
2020-07-17 18:55:29 -0600

    Reproduccion desde la fila

--- src/Home/RecomendacionesIniciales.js
+++ src/Home/RecomendacionesIniciales.js
@@ -13,7 +13,12 @@ class RecomendacionesIniciales extends Component {
                     name: "",
                     artist: ""
                 }
-            ]
+            ],
+            filas: (<tr>
+                <th colSpan="2">
+                    No se pudieron recomendar canciones
+                </th>
+            </tr>)
         }
         if (localStorage.getItem("shiftemotiontoken") !== null && localStorage.getItem("user-email") !== null) {
             Axios.get('https://8m717iy4bh.execute-api.us-east-1.amazonaws.com/Prod/recomendaciones/home?email=' + localStorage.getItem("user-email"))
@@ -36,6 +41,28 @@ class RecomendacionesIniciales extends Component {
         }
     }
 
+    renderRecommendation() {
+        try {
+            const contenido = this.state.recomendaciones.map((track) => {
+                return (<tr key={track.id}>
+                   <td>{track.name}</td>
+                   <td>{track.artist}</td>
+               </tr>)
+           })
+            this.setState({
+                filas: contenido
+            })
+        } catch (e) {
+            this.setState({
+                filas: (<tr>
+                    <th colSpan="2">
+                        No se pudieron recomendar canciones
+                    </th>
+                </tr>)
+            })
+        }
+    }
+
     render() {
         return (<Card>
             <Card.Body>
@@ -49,11 +76,16 @@ class RecomendacionesIniciales extends Component {
                     <tbody>
                         {
                             this.state.recomendaciones.map((track) => {
-                                return (<tr key={track.id}>
-                                    <td>{track.name}</td>
-                                    <td>{track.artist}</td>
-                                </tr>)
-                            })
+                                return (<tr key={track.id} onClick={
+                                    () => {
+                                        var win = window.open("https://open.spotify.com/track/" + track.id, '_blank');
+                                        win.focus();
+                                    }
+                                }>
+                                   <td>{track.name}</td>
+                                   <td>{track.artist}</td>
+                               </tr>)
+                           })
                         }
                     </tbody>
                 </Table>
